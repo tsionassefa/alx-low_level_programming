1fe43469c02b0285154abd370470b4246418cbcd
tsionassefa <tsionassefa101@gmail.com>
2022-10-13 12:33:07 -0700

    hi

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-write(21, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+write(21, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
