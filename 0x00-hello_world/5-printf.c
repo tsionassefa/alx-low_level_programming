ad9873717ca4eab37e648e42d466db4d3df55b5d
tsionassefa <tsionassefa101@gmail.com>
2022-10-13 10:07:12 -0700

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  *
  *  * main - Entry point
@@ -10,6 +12,8 @@
  */
 int main (void)
 {
+
             printf("with proper grammar, but the outcome is a piece of art,\n");
             return 0;
+
 }
