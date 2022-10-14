0b338b39fc1f75b8f5bdc6c7696d5924c5f770f6
tsionassefa <tsionassefa101@gmail.com>
2022-10-14 08:37:44 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-return lower case of alphabet
+ * return- 0 on success
+*/
+int main(void)
+{
+char tsi = 'a';
+while (a <= z)
+{
+putchar(tsi);
+tsi++;
+}
+putchar('\n')
+return 0;
+}
