9cc7931a8c44408cb8ad5d991f74a9f07e92af2c
tsionassefa <tsionassefa101@gmail.com>
2022-10-19 03:59:16 -0700

    hi

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * entry-program entry
+ * Description-display time table
+ * main-return 0 always
+ */
+void print_times_table(int n)
+{
+int i = n;
+for(x = 0; x <= i; x++)
+{
+int pr = 0;
+_putchar("%d", 0)
+for(y = 0; y < i; y++)
+{
+pr = pr + x;
+}
+_putchar('\n');
+}
+print_times_table(3);
+print_times_table(5);
+print_times_table(98);
+print_times_table(12);
+return (0);
+}
