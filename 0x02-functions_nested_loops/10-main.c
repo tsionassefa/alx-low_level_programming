53f99f9e4ca76158db3577b054925070ebe9d0ae
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2022-10-19 05:17:57 +0300

    Update 10-main.c

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,14 @@
+julien@ubuntu:~/$ cat 10-main.c
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+ n = add(89, 9);
+ printf("%d\n", n);
+ return (0);
+}
