965a72fb07a55dd5078b6cce6b8c2344f403bca1
tsionassefa <tsionassefa101@gmail.com>
2022-10-20 12:31:49 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - check the code for multiplication
+ * @c: the character to be checked
+ * Return: Always 0.
+ */
+int mul(int a, int b);
+{
+	return(a * b);
+}
