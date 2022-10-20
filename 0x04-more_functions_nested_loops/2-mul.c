8b9cd9631791a1c3740c56c6f9a362fd1baf2468
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2022-10-20 22:34:42 +0300

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
+
 /**
- * mul - check the code for multiplication
- * @c: the character to be checked
+ * mul - check the code for mul.
+ * @a: First integer
+ * @b: Second integer
  * Return: Always 0.
  */
-int mul(int a, int b);
+int mul(int a, int b)
 {
-	return(a * b);
+	return (a * b);
 }
