b8ead7150a408d2767be5551ae588b5aed44938f
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2022-10-20 23:18:02 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - check the code for to print numbers.
+ * print_numbers - check the code for print numbers.
  *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 48; i <= 57; i++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		_putchar(i);
+		_putchar(num);
 	}
 	_putchar('\n');
 }
