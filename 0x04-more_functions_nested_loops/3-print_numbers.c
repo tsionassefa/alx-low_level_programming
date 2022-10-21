77aa688a2e787e3fb9471929411fec83913fc38a
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2022-10-21 08:40:19 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_numbers - prints numbers between 0 to 9.
- * Return: no return.
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
 void print_numbers(void)
 {
-	int i;
-	for(i >= 48; i <=57; i++)
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
 	{
-		_putchar(i);
-	} 
+		_putchar(num);
+	}
 	_putchar('\n');
-}
+}}
