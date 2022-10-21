6458e7332dd0b1ef22f3290cca02731ec02c6d30
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2022-10-21 08:45:53 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
  */
 void print_numbers(void)
 {
-	int num;
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+_putchar(num);
+}
+_putchar('\n');
+}
 
-	for (num = '0'; num <= '9'; num++)
-	{
-		_putchar(num);
-	}
-	_putchar('\n');
-}}
