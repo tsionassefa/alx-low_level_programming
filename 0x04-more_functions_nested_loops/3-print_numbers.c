7ad021a60b6ae4f32a806aed9e9d9cf2b0a80b92
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2022-10-21 14:16:36 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_numbers - check the code to print 0-9.
  *
@@ -8,7 +7,6 @@
 void print_numbers(void)
 {
 int num;
-
 for (num = '0'; num <= '9'; num++)
 {
 _putchar(num);
