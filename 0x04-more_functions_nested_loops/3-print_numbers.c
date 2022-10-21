f1b71eed34efb7519df0b4b24ab597f36fe3d51a
tsionassefa <tsionassefa101@gmail.com>
2022-10-20 23:02:58 -0700

    hi

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+	_putchar(num);
+	}
+	_putchar('\n');
+}
