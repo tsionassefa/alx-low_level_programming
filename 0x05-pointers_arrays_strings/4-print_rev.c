b2c745b895a2c235a3ee31aedb02edd627fe05ae
tsionassefa <tsionassefa101@gmail.com>
2022-10-24 08:34:16 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - check the code for reverse.
+ * @s: String to be reverse printed
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (i -= 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
