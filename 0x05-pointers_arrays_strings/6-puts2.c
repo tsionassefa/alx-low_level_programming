6fc8c926e7ab763a84c957edf7f9294956215f67
tsionassefa <tsionassefa101@gmail.com>
2022-10-24 09:27:41 -0700

    hi

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code.
+ * @str: String for alternate printing
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	i--;
+
+	for (j = 0; j <= i; j++)
+	{
+		if (j % 2 == 0)
+			_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
