25c39061a1c323ed33707eae63a2963a12471c68
tsionassefa <tsionassefa101@gmail.com>
2022-10-24 06:03:43 -0700

    m

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - check the code to print the string.
+ * @str: String to be printed out.
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int f;
+
+	for (f =0; str[f] != '\0'; f++)
+		_putchar(str[f]);
+
+	_puchar('\n');
+}
