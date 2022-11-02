88713d0c0489fe6e0e4f65e3e1aac7ec3abc7078
tsionassefa <tsionassefa101@gmail.com>
2022-11-02 05:14:39 -0700

    hi

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,10 +7,10 @@
  */
 int factorial(int n)
 {
-	if (n <= 1 )
-		return (n);
-	else if (n < 0 )
+	if (n < 0)
 		return (-1);
-	else 
-		return (n * factorial(n -1));
+	else if (n < 2)
+		return (n);
+	else
+		return (n * factorial(n - 1));
 }
