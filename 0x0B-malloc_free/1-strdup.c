11446d030e3fe6079e883f22717880eab93da049
tsionassefa <tsionassefa101@gmail.com>
2022-11-07 12:54:36 +0300

    gi

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,20 +9,19 @@
 char *_strdup(char *str)
 {
 	char *a;
-	int i,c;
+	int i, c;
 
 	if (str == NULL)
-		return(NULL);
+		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++);
 	
-	a = malloc(i * sizeof(*a)+1);
+	a = malloc(i * sizeof(*a) + 1);
 	if (a == NULL)
 		return (NULL);
 
 	for (c = 0; c < i; c++)
 		a[c] = str[c];
 	a[c] = '\0';
-
 	return (a);
 }	
