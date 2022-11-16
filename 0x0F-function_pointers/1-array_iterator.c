7c867cb239726071b4cb6c1fb6b1dfa5812741b7
tsionassefa <tsionassefa101@gmail.com>
2022-11-16 14:57:24 +0300

    hi

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,6 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
